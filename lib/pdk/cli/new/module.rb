c580b8e0c4aa8ba0d6352458753cbc98ef40f708
David Schmitt <david.schmitt@puppet.com>
2017-04-19 11:08:04 +0100

    (SDK-178) Update module_interview to skip license, if supplied

--- lib/pdk/cli/new/module.rb
+++ lib/pdk/cli/new/module.rb
@@ -17,13 +17,13 @@ module PDK
 
             option nil, 'template-url', 'Overrides the template to use for this module.', argument: :required
 
-            option nil, 'license', 'Specifies the license this module is written under.', argument: :required
+            option nil, 'license', 'Specifies the license this module is written under. This should be a identifier from https://spdx.org/licenses/. Common values are "Apache-2.0", "MIT", or "proprietary".', argument: :required
 
             option nil, 'vcs', 'Specifies the version control driver. Valid values: git, none. Default: git.', argument: :required
 
             run do |opts, args, cmd|
               puts "Creating new module: #{args[0]}"
-              PDK::Generate::Module.invoke(args[0])
+              PDK::Generate::Module.invoke(args[0], opts)
             end
           end
         end
--- lib/pdk/generators/module.rb
+++ lib/pdk/generators/module.rb
@@ -14,23 +14,25 @@ module PDK
       end
 
       def self.invoke(name, opts={})
-        metadata = PDK::Module::Metadata.new(
-          {
-            'name' => name,
-            'version' => '0.1.0',
-            'dependencies' => [
-              { 'name' => 'puppetlabs-stdlib', 'version_requirement' => '>= 1.0.0' }
-            ]
-          }
-        )
+        defaults = {
+          'name' => name,
+          'version' => '0.1.0',
+          'dependencies' => [
+            { 'name' => 'puppetlabs-stdlib', 'version_requirement' => '>= 1.0.0' }
+          ]
+        }
 
-        module_interview(metadata) unless opts[:skip_interview] # TODO: Build way to get info by answers file
+        defaults['license'] = opts[:license] if opts.has_key? :license
+
+        metadata = PDK::Module::Metadata.new(defaults)
+
+        module_interview(metadata, opts) unless opts[:skip_interview] # TODO: Build way to get info by answers file
 
         # TODO: write metadata.json, build module directory structure, and write out templates.
         PDK::CLI::Exec.execute(cmd(opts))
       end
 
-      def self.module_interview(metadata)
+      def self.module_interview(metadata, opts={})
         puts "We need to create a metadata.json file for this module.  Please answer the"
         puts "following questions; if the question is not applicable to this module, feel free"
         puts "to leave it blank."
@@ -47,8 +49,10 @@ module PDK
         puts "\nWho wrote this module?  [#{metadata.data['author']}]"
         metadata.data.update('author' => PDK::CLI::Input.get(metadata.data['author']))
 
-        puts "\nWhat license does this module code fall under?  [#{metadata.data['license']}]"
-        metadata.data.update('license' => PDK::CLI::Input.get(metadata.data['license']))
+        if not opts.has_key? :license
+          puts "\nWhat license does this module code fall under?  [#{metadata.data['license']}]"
+          metadata.data.update('license' => PDK::CLI::Input.get(metadata.data['license']))
+        end
 
         puts "\nHow would you describe this module in a single sentence?"
         metadata.data.update('summary' => PDK::CLI::Input.get(metadata.data['summary']))
