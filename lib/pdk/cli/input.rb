02b14c05e3e37a49de00da8ebc3796d2a6cef4d1
Tim Sharpe <tim@sharpe.id.au>
2017-05-08 14:28:54 +1000

    Documentation and error handling

--- lib/pdk/cli/input.rb
+++ lib/pdk/cli/input.rb
@@ -1,6 +1,15 @@
 module PDK
   module CLI
     module Input
+      # Query the user for a value via STDIN.
+      #
+      # @param message [String] The message to be displayed to the user before
+      # accepting input.
+      # @param default [String] The default value to be used if the user
+      # provides a blank value.
+      #
+      # @return [String] The value provided by the user (or the supplied
+      # default value).
       def self.get(message, default=nil)
         print message
         if default.nil?
