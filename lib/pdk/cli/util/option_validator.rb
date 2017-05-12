c3bcb941b5c461dfcabcf3b8e70c26d6312fa7f0
Tim Sharpe <tim@sharpe.id.au>
2017-05-12 17:52:21 +1000

    Add method to OptionValidator to valid Puppet namespaces

--- lib/pdk/cli/util/option_validator.rb
+++ lib/pdk/cli/util/option_validator.rb
@@ -22,6 +22,17 @@ module PDK
         def self.is_valid_module_name?(string)
           !(string =~ /\A[a-z][a-z0-9_]*\Z/).nil?
         end
+
+        # Validate a Puppet namespace against the regular expression in the
+        # documentation: https://docs.puppet.com/puppet/4.10/lang_reserved.html#classes-and-defined-resource-types
+        def self.is_valid_namespace?(string)
+          return false if (string || '').split('::').last == 'init'
+
+          !(string =~ /\A([a-z][a-z0-9_]*)?(::[a-z][a-z0-9_]*)*\Z/).nil?
+        end
+
+        singleton_class.send(:alias_method, :is_valid_class_name?, :is_valid_namespace?)
+        singleton_class.send(:alias_method, :is_valid_defined_type_name?, :is_valid_namespace?)
       end
     end
   end
--- spec/cli/option_validator_spec.rb
+++ spec/cli/option_validator_spec.rb
@@ -63,4 +63,32 @@ describe PDK::CLI::Util::OptionValidator do
       expect(subject.is_valid_module_name?('testThing')).to be false
     end
   end
+
+  context 'when verifying a class name' do
+    it { is_expected.to respond_to(:is_valid_class_name?).with(1).argument }
+
+    it 'returns true if passed a lowercase word' do
+      expect(subject.is_valid_class_name?('test')).to be true
+    end
+
+    it 'returns true if passed a string containing only lowercase letters, digits, and underscores' do
+      expect(subject.is_valid_class_name?('test_123')).to be true
+    end
+
+    it 'returns true if passed a valid segmented namespace' do
+      expect(subject.is_valid_class_name?('testmodule::testclass')).to be true
+    end
+
+    it 'returns false if passed nil' do
+      expect(subject.is_valid_class_name?(nil)).to be false
+    end
+
+    it 'returns false if passed an empty string' do
+      expect(subject.is_valid_class_name?(nil)).to be false
+    end
+
+    it 'returns false if passed init' do
+      expect(subject.is_valid_class_name?('init')).to be false
+    end
+  end
 end
