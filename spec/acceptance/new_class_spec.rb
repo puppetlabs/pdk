e5897544973e7c170696e900bc3c0cb3baaffdc1
james-stocks <james.stocks@puppetlabs.com>
2017-06-14 10:23:01 +0000

    (SDK-190) Acceptance tests for using commands outside module folder
    Adds an acceptance spec for using pdk new class when not inside a module folder.
    Adds a pending acceptance spec for using pdk test unit when not inside a module folder.
    Does not address pdk validate when not inside a module folder - SDK-277 is an existing bug for this.

--- spec/acceptance/new_class_spec.rb
+++ spec/acceptance/new_class_spec.rb
@@ -1,33 +1,42 @@
 require 'spec_helper_acceptance'
 
 describe 'Creating a new class' do
-  include_context 'in a new module', 'foo'
+  context 'in a new module' do
+    include_context 'in a new module', 'foo'
 
-  context 'when creating the main class' do
-    describe command('pdk new class foo') do
-      its(:exit_status) { is_expected.to eq 0 }
-      its(:stdout) { is_expected.to match(%r{Creating .* from template}) }
-      its(:stdout) { is_expected.not_to match(%r{WARN|ERR}) }
-      # use this weird regex to match for empty string to get proper diff output on failure
-      its(:stderr) { is_expected.to match(%r{\A\Z}) }
-    end
+    context 'when creating the main class' do
+      describe command('pdk new class foo') do
+        its(:exit_status) { is_expected.to eq 0 }
+        its(:stdout) { is_expected.to match(%r{Creating .* from template}) }
+        its(:stdout) { is_expected.not_to match(%r{WARN|ERR}) }
+        # use this weird regex to match for empty string to get proper diff output on failure
+        its(:stderr) { is_expected.to match(%r{\A\Z}) }
+      end
 
-    describe file('manifests') do
-      it { is_expected.to be_directory }
-    end
+      describe file('manifests') do
+        it { is_expected.to be_directory }
+      end
 
-    describe file('manifests/init.pp') do
-      it { is_expected.to be_file }
-      its(:content) do
-        is_expected.to match(%r{class foo})
+      describe file('manifests/init.pp') do
+        it { is_expected.to be_file }
+        its(:content) do
+          is_expected.to match(%r{class foo})
+        end
+      end
+
+      describe file('spec/classes/foo_spec.rb') do
+        it { is_expected.to be_file }
+        its(:content) do
+          is_expected.to match(%r{foo})
+        end
       end
     end
+  end
 
-    describe file('spec/classes/foo_spec.rb') do
-      it { is_expected.to be_file }
-      its(:content) do
-        is_expected.to match(%r{foo})
-      end
+  context 'outside a module folder' do
+    describe command('pdk new class bar') do
+      its(:exit_status) { is_expected.not_to eq 0 }
+      its(:stdout) { is_expected.to match(%r{metadata.json.*does not exist}) }
     end
   end
 end
