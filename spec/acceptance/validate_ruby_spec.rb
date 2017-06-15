8d59bd2c786a3148ce239553f0253e2086891fcb
David Schmitt <david.schmitt@puppet.com>
2017-06-15 17:11:27 +0100

    (SDK-244) Basic acceptance tests for ruby validation

---
+++ spec/acceptance/validate_ruby_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper_acceptance'
+
+describe 'Validating a module' do
+  context 'with a fresh module' do
+    include_context 'in a new module', 'foo'
+
+    describe command('pdk validate ruby') do
+      its(:exit_status) { is_expected.to eq 0 }
+      its(:stdout) do
+        pending('correct output needs implementing')
+        # TODO: fixup to correct message
+        is_expected.to match(%r{Validating ruby style})
+      end
+      its(:stdout) { is_expected.not_to match(%r{WARN|ERR}) }
+      # use this weird regex to match for empty string to get proper diff output on failure
+      its(:stderr) { is_expected.to match(%r{\A\Z}) }
+    end
+  end
+
+  context 'with a style violation' do
+    include_context 'in a new module', 'foo'
+    before(:all) do
+      File.open('spec/violation.rb', 'w') do |f|
+        f.puts <<EOF
+f = %(x y z)
+EOF
+      end
+    end
+
+    describe command('pdk validate ruby') do
+      its(:exit_status) do
+        pending('correct reporting needs implementing')
+        is_expected.not_to eq 0
+      end
+      its(:stdout) do
+        pending('correct output needs implementing')
+        # TODO: fixup to correct message
+        is_expected.to match(%r{violation.rb sucks})
+      end
+      its(:stdout) { is_expected.not_to match(%r{WARN|ERR}) }
+      # use this weird regex to match for empty string to get proper diff output on failure
+      its(:stderr) { is_expected.to match(%r{\A\Z}) }
+    end
+  end
+end
