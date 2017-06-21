8f4d733a5ad49af6cb1712b8f155569ca9443106
David Schmitt <david.schmitt@puppet.com>
2017-06-21 10:27:27 +0100

    (maint) more `new class` acceptance tests

--- spec/acceptance/new_class_spec.rb
+++ spec/acceptance/new_class_spec.rb
@@ -20,7 +20,7 @@ describe 'Creating a new class' do
       describe file('manifests/init.pp') do
         it { is_expected.to be_file }
         its(:content) do
-          is_expected.to match(%r{class foo})
+          is_expected.to match(%r{class foo })
         end
       end
 
@@ -28,6 +28,62 @@ describe 'Creating a new class' do
         it { is_expected.to be_file }
         its(:content) do
           is_expected.to match(%r{foo})
+        end
+      end
+    end
+
+    context 'when creating an ancillary class' do
+      describe command('pdk new class foo::bar') do
+        its(:exit_status) { is_expected.to eq 0 }
+        its(:stdout) { is_expected.to match(%r{Creating .* from template}) }
+        its(:stdout) { is_expected.not_to match(%r{WARN|ERR}) }
+        # use this weird regex to match for empty string to get proper diff output on failure
+        its(:stderr) { is_expected.to match(%r{\A\Z}) }
+      end
+
+      describe file('manifests') do
+        it { is_expected.to be_directory }
+      end
+
+      describe file('manifests/bar.pp') do
+        it { is_expected.to be_file }
+        its(:content) do
+          is_expected.to match(%r{class foo::bar})
+        end
+      end
+
+      describe file('spec/classes/bar_spec.rb') do
+        it { is_expected.to be_file }
+        its(:content) do
+          is_expected.to match(%r{foo::bar})
+        end
+      end
+    end
+
+    context 'when creating a deeply nested class' do
+      describe command('pdk new class foo::bar::baz') do
+        its(:exit_status) { is_expected.to eq 0 }
+        its(:stdout) { is_expected.to match(%r{Creating .* from template}) }
+        its(:stdout) { is_expected.not_to match(%r{WARN|ERR}) }
+        # use this weird regex to match for empty string to get proper diff output on failure
+        its(:stderr) { is_expected.to match(%r{\A\Z}) }
+      end
+
+      describe file('manifests') do
+        it { is_expected.to be_directory }
+      end
+
+      describe file('manifests/bar/baz.pp') do
+        it { is_expected.to be_file }
+        its(:content) do
+          is_expected.to match(%r{class foo::bar::baz})
+        end
+      end
+
+      describe file('spec/classes/bar/baz_spec.rb') do
+        it { is_expected.to be_file }
+        its(:content) do
+          is_expected.to match(%r{foo::bar::baz})
         end
       end
     end
