197ce340e0f34c94084787c6e8d082cb349b43e9
Tim Sharpe <tim@sharpe.id.au>
2017-06-22 12:51:55 +1000

    (SDK-256) Add acceptance tests for metadata.json validation

---
+++ spec/acceptance/validate_metadata_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper_acceptance'
+
+describe 'Running metadata validation' do
+  let(:junit_xsd) { File.join(RSpec.configuration.fixtures_path, 'JUnit.xsd') }
+  let(:spinner_text) { %r{checking metadata\.json}i }
+
+  context 'with a fresh module' do
+    include_context 'in a new module', 'foo'
+
+    describe command('pdk validate metadata') do
+      its(:exit_status) { is_expected.to eq(0) }
+      its(:stdout) { is_expected.to match(%r{\A\Z}) }
+      its(:stderr) { is_expected.to match(spinner_text) }
+    end
+
+    describe command('pdk validate metadata --format junit') do
+      its(:exit_status) { is_expected.to eq(0) }
+      its(:stderr) { is_expected.to match(spinner_text) }
+      its(:stdout) { is_expected.to pass_validation(junit_xsd) }
+
+      its(:stdout) do
+        is_expected.to have_xpath('/testsuites/testsuite[@name="metadata-json-lint"]').with_attributes(
+          'failures' => '0',
+          'tests'    => '1',
+        )
+      end
+
+      its(:stdout) do
+        is_expected.to have_xpath('/testsuites/testsuite[@name="metadata-json-lint"]/testcase').with_attributes(
+          'classname' => 'metadata-json-lint',
+          'name'      => 'metadata.json',
+        )
+      end
+    end
+  end
+
+  context 'with a metadata violation' do
+    include_context 'in a new module', 'foo'
+
+    before(:all) do
+      metadata = JSON.parse(File.read('metadata.json'))
+      metadata['dependencies'].first['version_requirement'] = '>= 1.0.0'
+      File.open('metadata.json', 'w') do |f|
+        f.puts metadata.to_json
+      end
+    end
+
+    describe command('pdk validate metadata') do
+      its(:exit_status) { is_expected.not_to eq(0) }
+      its(:stdout) { is_expected.to match(%r{^metadata\.json:.+warning.+open ended dependency}) }
+      its(:stderr) { is_expected.to match(spinner_text) }
+    end
+
+    describe command('pdk validate metadata --format junit') do
+      its(:exit_status) { is_expected.not_to eq(0) }
+      its(:stderr) { is_expected.to match(spinner_text) }
+      its(:stdout) { is_expected.to pass_validation(junit_xsd) }
+
+      its(:stdout) do
+        is_expected.to have_xpath('/testsuites/testsuite[@name="metadata-json-lint"]').with_attributes(
+          'failures' => '1',
+          'tests'    => '1',
+        )
+      end
+
+      its(:stdout) do
+        is_expected.to have_xpath('/testsuites/testsuite[@name="metadata-json-lint"]/testcase').with_attributes(
+          'classname' => 'metadata-json-lint.dependencies',
+          'name'      => 'metadata.json',
+        )
+      end
+    end
+  end
+end
