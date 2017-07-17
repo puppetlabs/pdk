721aafef434b9d9ac54d995d5834b8127cb8464c
james-stocks <james.stocks@puppetlabs.com>
2017-07-17 17:02:42 +0000

    (maint) Update package testing to not use bundler
    Avoid using bundler and (more importantly) do not copy pdk lib over to the System Under Test.

--- package-testing/tests/run_acceptance.rb
+++ package-testing/tests/run_acceptance.rb
@@ -4,7 +4,7 @@ require 'pdk/pdk_helper.rb'
 step 'Run the tests' do
   # TODO: Need to add junit formatter to allow junit XML output for CI to parse
   # TODO: json formatter causes rspec to crash when encoding results in json
-  rspec_command = "#{command_prefix(workstation)} bundle exec rspec --pattern 'spec/acceptance/**.rb' --format documentation --out results.out"
+  rspec_command = "#{command_prefix(workstation)} rspec --pattern 'spec/acceptance/**/*_spec.rb' --format documentation --out results.out"
   on(workstation, rspec_command, accept_all_exit_codes: true) do |outcome|
     assert_equal('', outcome.stderr, 'rspec stderr should be blank')
     assert_equal(0, outcome.exit_code, 'rspec acceptance tests should exit with 0')
