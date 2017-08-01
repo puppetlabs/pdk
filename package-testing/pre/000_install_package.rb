201dac8c386d4944d1346575e3d902a509b07566
Jesse Scott <jesse@puppetlabs.com>
2017-08-01 14:37:23 -0700

    (MAINT) Add ability to test locally built package with beaker

--- package-testing/pre/000_install_package.rb
+++ package-testing/pre/000_install_package.rb
@@ -6,6 +6,10 @@ test_name 'Install pdk package on workstation host' do
       # BKR-1109 requests a neater way to install an MSI
       msi_url = "http://#{ENV['BUILD_SERVER']}/pdk/#{ENV['SHA']}/repos/windows/pdk-x64.msi"
       generic_install_msi_on(workstation, msi_url)
+    elsif ENV['LOCAL_PKG']
+      pkg_fullname = File.basename(ENV['LOCAL_PKG'])
+      scp_to(workstation, ENV['LOCAL_PKG'], "/root/#{pkg_fullname}")
+      workstation.install_package(pkg_fullname)
     else
       install_puppetlabs_dev_repo(workstation, 'pdk', ENV['SHA'], 'repo-config')
       workstation.install_package('pdk')
