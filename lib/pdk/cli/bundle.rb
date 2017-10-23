a50e844a7b1ba19f404ef07e69b5f5f5d67f5243
David Schmitt <david.schmitt@puppet.com>
2017-10-23 11:52:01 +0100

    (maint) Allow `pdk bundle` to work without `--`
    By upgrading to the new version of CRI, the `skip_option_parsing`
    call can be used to avoid the previous confusion of bundle vs pdk
    options.

--- lib/pdk/cli/bundle.rb
+++ lib/pdk/cli/bundle.rb
@@ -2,11 +2,7 @@
 module PDK::CLI
   @bundle_cmd = @base_cmd.define_command do
     name 'bundle'
-    if Gem.win_platform?
-      usage _('bundle `-- [bundler_options]')
-    else
-      usage _('bundle -- [bundler_options]')
-    end
+    usage _('bundle [bundler_options]')
     summary _('(Experimental) Command pass-through to bundler')
     description _(<<-EOF
 [experimental] For advanced users, pdk bundle runs arbitrary commands in the bundler environment that pdk manages.
@@ -15,6 +11,7 @@ Careless use of this command can lead to errors that pdk can't help recover from
 Note that for PowerShell the '--' needs to be escaped using a backtick: '`--' to avoid it being parsed by the shell.
 EOF
                  )
+    skip_option_parsing
 
     run do |_opts, args, _cmd|
       PDK::CLI::Util.ensure_in_module!(
