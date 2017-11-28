b8a7e37b553a8907e9e1843d55bd0d81105ccf73
Bryan Jen <bryan.jen@gmail.com>
2017-11-28 09:24:24 -0700

    (PDK-672) List files changed from convert

--- lib/pdk/module/convert.rb
+++ lib/pdk/module/convert.rb
@@ -1,5 +1,6 @@
 require 'pdk/generate/module'
 require 'pdk/module/update_manager'
+require 'pdk/util'
 
 module PDK
   module Module
@@ -8,12 +9,33 @@ module PDK
         # TODO: Dummy template metadata, replace with TemplateDir#metadata
         template_metadata = {}
         update_manager = PDK::Module::UpdateManager.new
+        template_url = options.fetch(:'template-url', PDK::Util.default_template_url)
 
         update_manager.modify_file('metadata.json', update_metadata('metadata.json', template_metadata))
 
+        PDK::Module::TemplateDir.new(template_url, nil, false) do |templates|
+          templates.render do |file_path, file_content|
+            if File.exist? file_path
+              update_manager.modify_file(file_path, file_content)
+            else
+              update_manager.add_file(file_path, file_content)
+            end
+          end
+        end
+
         return unless update_manager.changes?
 
+        [:added, :removed].each do |category|
+          PDK.logger.info(_('Files to be %{category}:') % { category: category })
+          update_manager.changes[category].each do |file|
+            # This is where we add the entries to the report
+            puts file[:path]
+          end
+        end
+
+        PDK.logger.info(_('Files to be modified:'))
         update_manager.changes[:modified].each do |_, diff|
+          # This is where we add the entries to the report
           puts diff
         end
 
