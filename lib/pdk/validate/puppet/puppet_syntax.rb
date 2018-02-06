f3842b353f57fd366c0da90121f521aee161fffe
Bryan Jen <bryan.jen@gmail.com>
2018-02-06 11:43:35 -0700

    (maint) Fixes output parsing from puppet syntax validation
    In Puppet >= 5.3.4, the error output from the Puppet Parser was changed
    to allow for better localization. This fix updates the built-in
    error output parser to work with the new format, but also adds fixes
    to parse variations of the legacy error output format.

--- lib/pdk/validate/puppet/puppet_syntax.rb
+++ lib/pdk/validate/puppet/puppet_syntax.rb
@@ -5,6 +5,25 @@ require 'pdk/validate/base_validator'
 module PDK
   module Validate
     class PuppetSyntax < BaseValidator
+      # In Puppet >= 5.3.4, the error context formatting was changed to facilitate localization
+      ERROR_CONTEXT = %r{(?:file:\s(?<file>.+?)|line:\s(?<line>.+?)|column:\s(?<column>.+?))}
+      # In Puppet < 5.3.3, the error context was formatted in these variations:
+      #   - "at file_path:line_num:col_num"
+      #   - "at file_path:line_num"
+      #   - "at line line_num"
+      #   - "in file_path"
+      ERROR_CONTEXT_LEGACY = %r{(?:at\sline\s(?<line>\d+)|at\s(?<file>.+?):(?<line>\d+):(?<column>\d+)|at\s(?<file>.+?):(?<line>\d+)|in\s(?<file>.+?))}
+
+      PUPPET_SYNTAX_PATTERN = %r{^
+        (?<severity>.+?):\s
+        (?<message>.+?)
+        (?:
+          \s\(#{ERROR_CONTEXT}(,\s#{ERROR_CONTEXT})*\)| # attempt to match the new localisation friendly location
+          \s#{ERROR_CONTEXT_LEGACY}| # attempt to match the old " at file:line:column" location
+          $                                               # handle cases where the output has no location
+        )
+      $}x
+
       def self.name
         'puppet-syntax'
       end
@@ -32,33 +51,12 @@ module PDK
       def self.parse_output(report, result, targets)
         # Due to PUP-7504, we will have to programmatically construct the json
         # object from the text output for now.
-        output = result[:stderr].split("\n")
+        output = result[:stderr].split("\n").reject { |entry| entry.empty? }
 
         results_data = []
         output.each do |offense|
-          sanitize_console_output(offense)
-          message, _at, location_raw = offense.partition(' at ')
-
-          # Parse the offense type and msg
-          severity, _colon, message = message.rpartition(': ')
-
-          # Parse the offense location info
-          location = location_raw.strip.match(%r{\A(?<file>.+):(?<line>\d+):(?<column>\d+)\Z}) unless location_raw.nil?
-
-          attributes = {
-            source:  name,
-            message: message.strip,
-            state:  :failure,
-          }
-          attributes[:severity] = severity.strip unless severity.nil?
-
-          unless location.nil?
-            attributes[:file] = location[:file]
-            attributes[:line] = location[:line]
-            attributes[:column] = location[:column]
-          end
-
-          results_data << attributes
+          offense_data = parse_offense(offense)
+          results_data << offense_data
         end
 
         # puppet parser validate does not include files without problems in its
@@ -76,6 +74,23 @@ module PDK
         results_data.each do |offense|
           report.add_event(offense)
         end
+      end
+
+      def self.parse_offense(offense)
+        sanitize_console_output(offense)
+
+        offense_data = {
+          source:  name,
+          state:  :failure,
+        }
+
+        attributes = offense.match(PUPPET_SYNTAX_PATTERN)
+
+        attributes.names.each do |name|
+          offense_data[name.to_sym] = attributes[name] unless attributes[name].nil?
+        end
+
+        offense_data
       end
 
       def self.sanitize_console_output(line)
