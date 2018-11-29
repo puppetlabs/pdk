a044ed44147e1b91d1518e724b792165bb7fd27d
Tim Sharpe <tim@bombasticmonkey.com>
2018-11-29 16:14:18 +1100

    (PDK-1204) pdk bundle execs in the context of the pwd
    Adds a new `:pwd` context to `PDK::CLI::Exec` that behaves the same as
    the `:module` context in that it sets up the Ruby environment to use the
    vendored libraries, but it runs the command from the working directory
    rather than the module root.

--- spec/acceptance/bundle_spec.rb
+++ spec/acceptance/bundle_spec.rb
@@ -4,11 +4,33 @@ describe 'pdk bundle' do
   context 'in a new module' do
     include_context 'in a new module', 'bundle'
 
+    before(:all) do
+      File.open(File.join('manifests', 'init.pp'), 'w') do |f|
+        f.puts '$foo = "bar"'
+      end
+    end
+
     describe command('pdk bundle env') do
       its(:exit_status) { is_expected.to eq 0 }
       # use this weird regex to match for empty string to get proper diff output on failure
       its(:stdout) { is_expected.to match(%r{\A\Z}) }
       its(:stderr) { is_expected.to match(%r{## Environment}) }
+    end
+
+    context 'when running in a subdirectory of the module root' do
+      before(:all) do
+        Dir.chdir('manifests')
+      end
+
+      after(:all) do
+        Dir.chdir('..')
+      end
+
+      describe command('pdk bundle exec puppet-lint init.pp') do
+        its(:exit_status) { is_expected.to eq(0) }
+        its(:stdout) { is_expected.to match(%r{\A\Z}) }
+        its(:stderr) { is_expected.to match(%r{double quoted string}im) }
+      end
     end
   end
 end
