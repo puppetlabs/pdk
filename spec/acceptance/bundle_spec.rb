12467933493e2eee3936ba74f258e4ffd5069809
Tim Sharpe <tim@bombasticmonkey.com>
2019-03-05 14:15:31 +1100

    (PDK-1298) Combine bundle_management_spec.rb and bundle_spec.rb

--- spec/acceptance/bundle_spec.rb
+++ spec/acceptance/bundle_spec.rb
@@ -31,5 +31,24 @@ describe 'pdk bundle' do
         its(:stderr) { is_expected.to match(%r{double quoted string}im) }
       end
     end
+
+    context 'when there is an invalid Gemfile' do
+      before(:all) do
+        FileUtils.mv('Gemfile', 'Gemfile.old', force: true)
+        File.open('Gemfile', 'w') do |f|
+          f.puts 'not a Gemfile'
+        end
+      end
+
+
+      after(:all) do
+        FileUtils.mv('Gemfile.old', 'Gemfile', force: true)
+      end
+
+      describe command('pdk bundle env') do
+        its(:exit_status) { is_expected.not_to eq(0) }
+        its(:stderr) { is_expected.to match(%r{error parsing `gemfile`}i) }
+      end
+    end
   end
 end
