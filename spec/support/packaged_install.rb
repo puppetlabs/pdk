2067abe3df03d9610d27a08f3554553d62789162
Glenn Sarti <glennsarti@users.noreply.github.com>
2019-06-07 13:54:39 +0800

    (PDK-1365) Use dynamic ruby detection for default ruby instance
    Previously the default version of ruby selected in packaged PDK instances was
    hardcoded to 2.4.5 which caused issues when updating ruby runtimes and did not
    reflect that the default should really by ruby 2.5 and Puppet 6.  This commit
    modifies the default ruby detection to instead use the ruby environment for the
    latest Puppet Gem in the PDK package install.  Non-pkged installs are not
    affected.
    
    This commit also memoizes the default value to slightly speed up default ruby
    selection.

--- spec/support/packaged_install.rb
+++ spec/support/packaged_install.rb
@@ -7,6 +7,7 @@ RSpec.shared_context 'packaged install' do
     allow(File).to receive(:exist?).with(%r{bundle(\.bat)?$}).and_return(true)
     allow(PDK::Util).to receive(:package_cachedir).and_return(package_cachedir)
     allow(PDK::Util::RubyVersion).to receive(:versions).and_return('2.4.4' => '2.4.0')
+    allow(PDK::Util::RubyVersion).to receive(:default_ruby_version).and_return('2.4.4')
   end
 end
 
