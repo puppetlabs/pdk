711a4a87282f9d6205da0c6e52fe53693be04389
Bert Hajee <bert.hajee@enterprisemodules.com>
2019-10-10 07:10:09 +0200

    Move the require pdk/utils from the top to where it is needed

--- lib/pdk/module/metadata.rb
+++ lib/pdk/module/metadata.rb
@@ -1,4 +1,3 @@
-require 'pdk/util'
 require 'pdk/util/filesystem'
 
 module PDK
@@ -109,6 +108,7 @@ module PDK
           raise ArgumentError, _('Invalid JSON in metadata.json: %{msg}') % { msg: e.message }
         end
 
+        require 'pdk/util'
         data['template-url'] = PDK::Util::TemplateURI.default_template_uri.metadata_format if PDK::Util.package_install? && data['template-url'] == PDK::Util::TemplateURI::PACKAGED_TEMPLATE_KEYWORD
         new(data)
       end
