10736a4e4536c53c10c86b3f287193ee15bf6994
Glenn Sarti <glennsarti@users.noreply.github.com>
2020-01-21 09:53:37 +0800

    (PDK-1590) Use short-name for PDK Package base directory on Windows
    Currently the Package Base Directory detection uses the full path to the PDK
    installation on Windows, however Bundler is very sensitive to spaces in the
    directory name [1] [2].  This commit modifies the pdk_package_basedir method
    to convert the path to its short name using the Windows GetShortPathNameW API
    call.
    
    Without this commit different Bundler versions on different Ruby versions may
    fail to run `pdk bundle ...` style calls.
    
    Note that the short path detection is not fool-proof.  For example if 8.3
    filename creation is disabled there is no shortname to find. In that case it
    just returns the long name.
    
    This code path is not explictly tested however it is implicitly tested in the
    package-testing tests as the PDK package is installed into "C:\Program Files\.."
    by default.
    
    Note that paths WITHIN the pdk are currently ok and don't require the API call
    as they don't have spaces in them.  It's just the path TO the PDK that could
    contain spaces.
    
    [1] https://github.com/rubygems/rubygems/issues/117
    [2] https://stackoverflow.com/questions/16898286/error-invalid-switch-in-rubyopt-f-runtimeerror-is-shown-while-install-gems

--- lib/pdk/util/windows/file.rb
+++ lib/pdk/util/windows/file.rb
@@ -1,6 +1,8 @@
 require 'pdk/util/windows'
 
 module PDK::Util::Windows::File
+  #:nocov:
+  # These are wrappers for system level APIs and therefore don't need to be tested
   require 'ffi'
   extend FFI::Library
   extend PDK::Util::Windows::String
@@ -23,6 +25,25 @@ module PDK::Util::Windows::File
   end
   module_function :get_long_pathname
 
+  # Taken from https://github.com/puppetlabs/puppet/blob/ba4d1a1aba0095d3c70b98fea5c67434a4876a61/lib/puppet/util/windows/file.rb
+  def get_short_pathname(path)
+    converted = ''
+    FFI::Pointer.from_string_to_wide_string(path) do |path_ptr|
+      # includes terminating NULL
+      buffer_size = GetShortPathNameW(path_ptr, FFI::Pointer::NULL, 0)
+      FFI::MemoryPointer.new(:wchar, buffer_size) do |converted_ptr|
+        if GetShortPathNameW(path_ptr, converted_ptr, buffer_size) == PDK::Util::Windows::WIN32_FALSE
+          raise _('Failed to call GetShortPathName')
+        end
+
+        converted = converted_ptr.read_wide_string(buffer_size - 1)
+      end
+    end
+
+    converted
+  end
+  module_function :get_short_pathname
+
   ffi_convention :stdcall
 
   # https://msdn.microsoft.com/en-us/library/windows/desktop/aa364980(v=vs.85).aspx
@@ -33,4 +54,14 @@ module PDK::Util::Windows::File
   # );
   ffi_lib :kernel32
   attach_function :GetLongPathNameW, [:lpcwstr, :lpwstr, :dword], :dword
+
+  # https://msdn.microsoft.com/en-us/library/windows/desktop/aa364989(v=vs.85).aspx
+  # DWORD WINAPI GetShortPathName(
+  #   _In_  LPCTSTR lpszLongPath,
+  #   _Out_ LPTSTR  lpszShortPath,
+  #   _In_  DWORD   cchBuffer
+  # );
+  ffi_lib :kernel32
+  attach_function_private :GetShortPathNameW, [:lpcwstr, :lpwstr, :dword], :dword
+  #:nocov:
 end
