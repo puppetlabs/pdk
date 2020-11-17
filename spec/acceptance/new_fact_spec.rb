f2e490f602b452f7a2a30495a579ff315e9d81d8
Corey Osman <corey@nwops.io>
2020-11-16 19:33:50 -0800

    Add a new generator for creating new facts
      * previously there was not a way to create facts
        from the new command.  This adds a generator to create
        the fact and spec file.

---
+++ spec/acceptance/new_fact_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper_acceptance'
+
+describe 'pdk new fact', module_command: true do
+  shared_examples 'it creates a fact' do |_options|
+    context 'in a new module' do
+      include_context 'in a new module', 'new_fact'
+
+      context 'when creating the fact' do
+        describe command('pdk new fact new_fact') do
+          its(:exit_status) { is_expected.to eq 0 }
+          its(:stdout) { is_expected.to match(%r{Files added}) }
+          its(:stdout) { is_expected.to match(%r{#{File.join('lib', 'facter', 'new_fact.rb')}}) }
+          its(:stdout) { is_expected.to match(%r{#{ File.join('spec', 'unit', 'facter', 'new_fact_spec.rb')}}) }
+          its(:stderr) { is_expected.to have_no_output }
+
+          it_behaves_like 'it creates a fact',
+                          name: 'new_fact',
+                          file: File.join('lib', 'facter', 'new_fact.rb'),
+                          spec: File.join('spec', 'unit', 'facter', 'new_fact_spec.rb')
+        end
+      end
+    end
+  end
+end
